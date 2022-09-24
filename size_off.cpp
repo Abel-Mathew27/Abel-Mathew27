cf45a259e2e21fe898197fee4d589ae2651dadad
Abel-Mathew27 <91581995+Abel-Mathew27@users.noreply.github.com>
2022-09-24 11:57:37 +0530

    second commit

--- size_off.cpp
+++ size_off.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 int main(){
-
+    // this sizeof function helps to find the size of the variable such as int, float, char, etc....
     cout<<"size of charecter: "<<sizeof(char)<<"byte"<<endl;
     cout<<"size of intrger: "<<sizeof(int)<<"bytes"<<endl;
     cout<<"size of float: "<<sizeof(float)<<"bytes"<<endl;
     cout<<"size of double: "<<sizeof(double)<<"bytes"<<endl;
+
+    cout <<"hello world";
 
     return 0;
 }
